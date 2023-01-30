79aa07b3a712a9dcdc4fe76aab09b0fb92096fb2
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-01-30 13:42:37 +0300

    Update easy_vector1.cpp

--- easy_vector1.cpp
+++ easy_vector1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){ //1
     for(int i = 0; i < mass.size(); i++){
         if (i % 2 == 0){
-            mass.push_back(mass[i]);
+            mass2.push_back(mass[i]);
         }
     }
 }
@@ -14,7 +14,7 @@ void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){ //2
     int a = 0;
     for(int i = 0; i < mass.size(); i++){
         if (i % 2 == 0){
-            mass[i] = mass2[a];
+            mass2[i] = mass[a];
             a++;
         }
     }
