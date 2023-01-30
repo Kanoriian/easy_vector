961eb6fe2b6e927230f317d7b6b666ca8825dff4
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-01-30 13:59:49 +0300

    Update easy_vector1.cpp

--- easy_vector1.cpp
+++ easy_vector1.cpp
@@ -19,3 +19,12 @@ void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){ //2
         }
     }
 }
+
+int itc_positive_list(const vector <int> &mass){ //3
+    int kolvo = 0;
+    for(int i = 0; i < mass.size(); i++){
+        if(mass[i] % 2 == 0)
+            kolvo++;
+    }
+    return kolvo;
+}
