172d87fded68d6528fb6256fee4dd8368ed2d0a1
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-01-30 15:16:08 +0300

    Update easy_vector2.cpp

--- easy_vector2.cpp
+++ easy_vector2.cpp
@@ -2,3 +2,10 @@
 
 using namespace std;
 
+void itc_rev_list(vector <int> &mass){ //6
+    int k;
+    for(int i = mass.size(); i > 0; i++){
+        k = mass[i-1];
+        mass[i-1] = mass[i];
+    }
+}
