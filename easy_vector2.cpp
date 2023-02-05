6b5d6722f209412d1f53e909c3f7beaca4e61d6c
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-02-05 14:32:35 +0300

    Update easy_vector2.cpp

--- easy_vector2.cpp
+++ easy_vector2.cpp
@@ -9,3 +9,34 @@ void itc_rev_list(vector <int> &mass){ //6
         mass[i-1] = mass[i];
     }
 }
+
+void itc_rev_par_list(vector <int> &mass){ //7
+    vector <int> mass2;
+    int a = 1;
+    for(int i = 0; i < mass.size(); i++){
+        mass2[i] = mass[a];
+        mass2[a] = mass[i];
+    }
+    mass = mass2;
+}
+
+void itc_rshift_list(vector <int> &mass){ //8
+    vector <int> mass2;
+    int a;
+    for(int i = 1; i < mass.size() - 1; i++){
+        a = i - 1;
+        mass2[a] = mass[i];
+    }
+    mass2[0] = mass[a+2];
+    mass = mass2;
+}
+
+void itc_super_shift_list(vector <int> &mass, int n){ //9
+    if(n > 0){
+        mass = itc_rev_par_list(mass);
+    }else{
+        for(int i = 0; i < mass.size() - 1; i++){
+            mass = itc_rev_par_list(mass);
+        }
+    }
+}
