39591258688fa44690dff08f996a540af426a1fc
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-02-06 13:33:04 +0300

    Add files via upload

--- easy_vector1.cpp
+++ easy_vector1.cpp
@@ -10,7 +10,7 @@ void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){ //1 +
     }
 }
 
-void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){ //2
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){ //2 +
     int a = 0;
     for(int i = 0; i < mass.size(); i++){
         if (mass[i] % 2 == 0){
@@ -20,7 +20,10 @@ void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){ //2
     }
 }
 
-int itc_positive_list(const vector <int> &mass){ //3
+int itc_positive_list(const vector <int> &mass){ //3 -
+    if(mass.size() = 0){
+        return 0;
+    }
     int kolvo = 0;
     for(int i = 0; i < mass.size(); i++){
         if(mass[i] % 2 == 0)
@@ -29,7 +32,10 @@ int itc_positive_list(const vector <int> &mass){ //3
     return kolvo;
 }
 
-int itc_sl_list(const vector <int> &mass){ //4
+int itc_sl_list(const vector <int> &mass){ //4 -
+    if(mass.size() = 0){
+        return 0;
+    }
     int kol = 0;
     for(int i = 0; i < mass.size(); i++){
         if(mass[i] < mass[i+1])
@@ -38,7 +44,10 @@ int itc_sl_list(const vector <int> &mass){ //4
     return kol;
 }
 
-bool itc_same_parts_list(const vector <int> &mass){ //5
+bool itc_same_parts_list(const vector <int> &mass){ //5 -
+    if(mass.size() = 0){
+        return 0;
+    }
     int kol = 0;
     for(int i = 0; i < mass.size(); i++){
         if(mass[i] / mass[i] == mass[i+1] / mass[i+1])
